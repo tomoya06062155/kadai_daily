7b792c4e3d17beca2b28349d1679c5ee9e5fedfa
tomoya.hasegawa <hasegawa06062155@gmail.com>
2020-10-24 20:29:32 +0900

    Add Report.java

--- src/models/Report.java
+++ src/models/Report.java
@@ -55,6 +55,10 @@ public class Report {
     @Column(name = "content", nullable = false)
     private String content;
 
+    @Lob
+    @Column(name = "comment", nullable = false)
+    private String comment;
+
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
@@ -99,6 +103,14 @@ public class Report {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public String getComment(){
+        return comment;
+    }
+
+    public void setComment(String comment){
+        this.comment = comment;
     }
 
     public Timestamp getCreated_at() {
